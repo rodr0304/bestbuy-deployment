d9171ce06d27bdaf6fb0f1c89edca6e9b8536ba5
Diniz Martins <rodr0304@algonquinlive.com>
2026-04-19 10:27:46 -0400

    Update server.js

--- product-service/server.js
+++ product-service/server.js
@@ -9,7 +9,7 @@ const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017";
 const DB_NAME = process.env.DB_NAME || "bestbuy";
 const COLLECTION = "products";
 
-//test
+//test1
 
 let db;
 
